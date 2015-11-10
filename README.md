7667a9408338eb5270062dedc5b89e310016461a
Viral Amin <vmanamino@gmail.com>
2015-11-10 03:39:59 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+**Django Project**
+
+- Integrating Facebook into app
+
+  - Django 1.8.6
+  - Python 2.7.6
---
+++ namequizzes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Quiz(models.Model):
+  name = models.CharField(max_length=200)
+  username = models.CharField(max_length=200)
+  def __unicode__(self):
+    return self.name
+
