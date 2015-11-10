394e90e9552c709395d2d5f8bad6fc0d9bbc0741
Viral Amin <vmanamino@gmail.com>
2015-11-10 06:12:42 +0000

    views, urls

--- namequizzes/urls.py
+++ namequizzes/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [ url(r'^$', views.index, name='index') ]
+urlpatterns = [
+    # /namequizzes/
+    url(r'^$', views.index, name='index'),
+    # /namequizzes/2/
+    url(r'^(?P<quiz_id>[0-9]+)/$', views.detail, name='detail'),
+  ]
--- namequizzes/views.py
+++ namequizzes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404, render
+from .models import Quiz
 
-# Create your views here.
+def index(request):
+  quizzes = Quiz.objects.all()
+  template = loader.get_template('namequizzes/index.html')
+  context = { 'quizzes': quizzes }
+  return render(request, 'namequizzes/index.html', context)
+
+def detail(request, quiz_id):
+  quiz = get_object_or_404(Quiz, pk=quiz_id)
+  return render(request, 'namequizzes/detail.html', {'quiz': quiz})
